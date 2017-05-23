0aad441b5d26499735fcdeec50153d67a85664f2
Walter Holladay <wholladay@simplifile.com>
2017-05-23 08:35:06 -0600

    Display the list of movies, anonymous login.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Observable} from 'rxjs/Observable';
+import {User} from 'firebase/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    user: Observable<User>;
+    videos: FirebaseListObservable<any[]>;
+
+    constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+        this.user = this.afAuth.authState;
+        this.afAuth.authState.subscribe(auth => {
+            if (auth) {
+                this.videos = af.list('/videos');
+            } else {
+                this.videos = null;
+            }
+        });
+    }
+
+    login() {
+        this.afAuth.auth.signInAnonymously();
+    }
+
+    logout() {
+        this.afAuth.auth.signOut();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCQEVOeFUTFGGqaTrLVNlfx47JPEdD3SR4',
+    authDomain: 'lazy-h-movies.firebaseapp.com',
+    databaseURL: 'https://lazy-h-movies.firebaseio.com',
+    projectId: 'lazy-h-movies',
+    storageBucket: 'lazy-h-movies.appspot.com',
+    messagingSenderId: '733231834753'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
